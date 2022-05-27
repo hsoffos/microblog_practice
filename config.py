66e26b83a04aa4b5e82d85982a15d9655d9f7af1
hsoffos <huntersoffos@gmail.com>
2022-05-27 19:55:28 -0400

    Pagination implemented in explore, home, profile sections

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']  # Error reports sent here
-
+    POSTS_PER_PAGE = 10
