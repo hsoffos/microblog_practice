35e132b167b1aef03d52d8282b47afe66e43ca57
hsoffos <huntersoffos@gmail.com>
2022-05-27 20:02:03 -0400

    set os environ variables, configured admin emails for error handling

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']  # Error reports sent here
+    ADMINS = ['MAIL_EMAIL']  # Error reports sent here
     POSTS_PER_PAGE = 10
