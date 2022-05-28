b890a40bb7b3341b3338e79bc41bf77952f1cdae
hsoffos <huntersoffos@gmail.com>
2022-05-28 14:26:22 -0400

    displaying times in an appealing fashion using moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -18,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 
 
 from app import routes, models, errors
