5c717c59eb0d0549fbcb81417d699484fcba6d5f
hsoffos <huntersoffos@gmail.com>
2022-05-28 13:40:28 -0400

    base template extends bootstrap template, updated jinja block content in all templates to reflect %block app_content% (bootstrap uses %block content%, so need to name something else)

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from config import Config
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -11,11 +12,12 @@ import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-mail = Mail(app)
-migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+migrate = Migrate(app, db)
 
 
 from app import routes, models, errors
